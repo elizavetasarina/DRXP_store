ecca8b6562448426d656a5136a000024746fd267
elizavetaatis <eliz.sarina@yandex.ru>
2026-06-17 13:14:53 +0300

    fix prisma config types

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,22 +1,13 @@
 import path from 'node:path'
 import { defineConfig } from 'prisma/config'
-import { PrismaPg } from '@prisma/adapter-pg'
 import { config } from 'dotenv'
 
 config()
 
-const directUrl = process.env.DIRECT_URL ?? process.env.DATABASE_URL ?? ''
-const databaseUrl = process.env.DATABASE_URL ?? ''
-
 export default defineConfig({
   schema: path.join(__dirname, 'prisma/schema.prisma'),
   datasource: {
-    url: databaseUrl,
-  },
-  migrate: {
-    adapter: () => {
-      return new PrismaPg({ connectionString: directUrl, ssl: { rejectUnauthorized: false } })
-    },
+    url: process.env.DATABASE_URL ?? '',
   },
   migrations: {
     seed: 'tsx prisma/seed.ts',
